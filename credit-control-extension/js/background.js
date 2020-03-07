d98e37dc987c324aab6b35c28761fd3e2c468cbf
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-03-07 18:55:44 -0500

    Notifications are hard :(

--- credit-control-extension/js/background.js
+++ credit-control-extension/js/background.js
@@ -1,11 +1,11 @@
+chrome.runtime.onMessage.addListener(function () {
+    const options = {
+        type: "basic",
+        iconUrl: "../images/icon48.png",
+        title: "This is the title",
+        message: "This is the main message of the notification",
 
+    };
 
-const options = {
-    type: "basic",
-    iconUrl: "../images/icon48.png",
-    title: "This is the title",
-    message: "This is the main message of the notification",
-
-};
-
-chrome.notifications.create(options);
+    chrome.notifications.create('id', options, function() { console.log("Last error:", chrome.runtime.lastError); });
+});
---
+++ credit-control-extension/js/popup.js
@@ -0,0 +1,11 @@
+const options = {
+    type: "basic",
+    iconUrl: "../images/icon48.png",
+    title: "This is the title",
+    message: "This is the main message of the notification",
+
+};
+
+console.log('here');
+
+chrome.notifications.create('id', options, function() { console.log("Last error:", chrome.runtime.lastError); });
