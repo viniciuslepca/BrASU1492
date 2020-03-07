757b03af666c30b990239ea1856376a067a824c5
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-03-07 16:40:03 -0500

    Trying to set up notifications

---
+++ credit-control-extension/js/background.js
@@ -0,0 +1,11 @@
+
+
+const options = {
+    type: "basic",
+    iconUrl: "../images/icon48.png",
+    title: "This is the title",
+    message: "This is the main message of the notification",
+
+};
+
+chrome.notifications.create(options);
--- credit-control-extension/js/content.js
+++ credit-control-extension/js/content.js
@@ -1,9 +1,3 @@
-const options = {
-    type: "basic",
-    iconUrl: "../images/icon48.png",
-    title: "This is the title",
-    message: "This is the main message of the notification",
-
-};
-
-chrome.notifications.create(options);
+chrome.runtime.sendMessage({
+    url: window.location.href,
+});
