045331f9c3ea2500552e95adf464f484188b3123
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-03-08 00:39:27 -0500

    None of this works, but alas

---
+++ credit-control-extension/js/components/PopupComponents.js
@@ -0,0 +1,9 @@
+class Application extends React.Component {
+    render() {
+        return (
+          <p>This is Nubank's Credit Control Extension!</p>
+        );
+    }
+}
+
+ReactDOM.render(<Application/>, document.querySelector("#popup"));
