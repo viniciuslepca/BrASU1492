a9ce43737291aa61b409be2683a4dd3a77465194
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-03-08 16:32:27 -0400

    Finally figured out how to obtain total price from checkout page

--- credit-control-extension/js/content.js
+++ credit-control-extension/js/content.js
@@ -1,7 +1,7 @@
 // const itemDetailsDiv = document.getElementsByClassName("a-fixed-left-grid-col item-details-right-column a-col-right");
 // const stuff = itemDetailsDiv[0].getElementsByClassName("a-text-bold");
 
-const price = document.getElementById("subtotals-marketplace-table").getElementsByClassName("grand-total-price").textContent;
+const price = document.getElementById("subtotals-marketplace-table").getElementsByClassName("grand-total-price")[0].textContent;
 
 chrome.runtime.sendMessage({
     price: price
---
+++ credit-control-extension/js/popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    function onclick () {
+      chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+      })
+    }
+
+    function setCount (res) {
+      const div = document.createElement('div')
+      div.textContent = `${res.count} bears`
+      document.body.appendChild(div)
+    }
+}, false)
