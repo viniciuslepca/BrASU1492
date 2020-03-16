bd1524d9222aa82b6875182c920becdd7e72cacc
Vinicius Lepca <vinicius.lepca@gmail.com>
2020-03-16 13:01:39 -0300

    Notification will display from navigation into amazon checkout page

--- credit-control-extension/js/content.js
+++ credit-control-extension/js/content.js
@@ -1,41 +1,44 @@
 // const itemDetailsDiv = document.getElementsByClassName("a-fixed-left-grid-col item-details-right-column a-col-right");
 // const stuff = itemDetailsDiv[0].getElementsByClassName("a-text-bold");
 
-const price = document.getElementById("subtotals-marketplace-table").getElementsByClassName("grand-total-price")[0].textContent;
-const priceVal = parseFloat(price.trim().substr(1));
 
-chrome.runtime.sendMessage({
-    price: price
-});
+if (location.href.includes("/buy/")) {
+    const price = document.getElementById("subtotals-marketplace-table").getElementsByClassName("grand-total-price")[0].textContent.trim();
+    const priceVal = parseFloat(price.substr(1));
 
-
-if (priceVal >= 30) {
-    const priceNotification = window.createNotification({
-        // close on click
-        closeOnClick: false,
-
-        // displays close button
-        displayCloseButton: true,
-
-        // nfc-top-left
-        // nfc-bottom-right
-        // nfc-bottom-left
-        positionClass: 'nfc-top-right',
-
-        // callback
-        onclick: false,
-
-        // timeout in milliseconds
-        showDuration: 10000,
-
-        // success, info, warning, error, and none
-        theme: 'info'
+    chrome.runtime.sendMessage({
+        price: price
     });
 
-    priceNotification({
-        title: 'Wait a second!',
-        message: 'This purchase represents X% of your monthly income. Click on the Nubank extension icon to learn more about it.'
-    });
+
+    if (priceVal >= 30) {
+        const priceNotification = window.createNotification({
+            // close on click
+            closeOnClick: false,
+
+            // displays close button
+            displayCloseButton: true,
+
+            // nfc-top-left
+            // nfc-bottom-right
+            // nfc-bottom-left
+            positionClass: 'nfc-top-right',
+
+            // callback
+            onclick: false,
+
+            // timeout in milliseconds
+            showDuration: 10000,
+
+            // success, info, warning, error, and none
+            theme: 'info'
+        });
+
+        priceNotification({
+            title: 'Wait a second!',
+            message: 'This purchase represents X% of your monthly income. Click on the Nubank extension icon to learn more about it.'
+        });
+    }
 }
 
 // chrome.runtime.onMessage.addListener(function(request, sender) {
